f85b7e3859d2128dbb4077324b501d7ed07f6f08
cornelius-weigert <cornelius.weigert@uni-bayreuth.de>
2025-11-06 11:20:13 +0100

    loading a bigger file now, still works -yay

--- practice/pm4py_test.py
+++ practice/pm4py_test.py
@@ -1,14 +1,15 @@
 import pm4py
 
 def main():
-    log = pm4py.read_xes('C:/Users/Corne/Dokumente/studium/Semester 5/SWPS/practice/running-example.xes')
+    # use a raw string so backslashes are not treated as escapes
+    log = pm4py.read_xes(r'C:\Users\Corne\Dokumente\studium\Semester 5\SWPS\practice\BPI_Challenge_2019.xes')
 
     # Discover the directly follows graph
     directly_follows_graph, start_activities, end_activities = pm4py.discover_dfg(log)
     bpmn= pm4py.discover_bpmn_inductive(log)
     # Visualize the DFG
     pm4py.view_dfg(directly_follows_graph, start_activities, end_activities,format='svg',graph_title="Directly Follows Graph")
-    pm4py.view_bpmn(bpmn,format='svg',graph_title="Hello World BPMN")
+    #pm4py.view_bpmn(bpmn,format='svg',graph_title="Hello World BPMN")
 
 def get_dfg_image(log):
     directly_follows_graph, start_activities, end_activities = pm4py.discover_dfg(log)
