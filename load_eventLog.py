a58c4b68b71b8bf7f9bff6114337ce0651cb3f93
cornelius-weigert <cornelius.weigert@uni-bayreuth.de>
2025-11-11 16:03:38 +0100

    created a module to load Eventlog from XES or CSV File

--- load_eventLog.py
+++ load_eventLog.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import pm4py
 
-def load_eventLog_from_csv(path, separator=',', case_id='Case ID', activity_key='Activity', timestamp_key='Complete Timestamp'):
+def eventLog_from_csv(path, separator=',', case_id='Case ID', activity_key='Activity', timestamp_key='Complete Timestamp'):
     dataframe = pd.read_csv(path, sep=',')
     dataframe = pm4py.format_dataframe(dataframe, case_id=case_id, activity_key=activity_key, timestamp_key=timestamp_key)
     log = pm4py.convert_to_event_log(dataframe)
     return log
 
+def eventLog_from_xes(path):
+    log = pm4py.read_xes(path)
+    return log
+
 def main():
-    log = load_eventLog_from_csv('Eventlogs/eventlog.csv')
+    log = eventLog_from_csv('Eventlogs/eventlog.csv')
     dfg,sa,ea = pm4py.discover_dfg(log)
     pm4py.view_dfg(dfg,sa,ea,format='svg',graph_title="Directly Follows Graph from CSV Log")
 
