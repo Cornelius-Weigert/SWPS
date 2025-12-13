e910364d82a38f83ba9b18bbc4d31feed370f279
Yaxin Wei <168448752+yxnice22@users.noreply.github.com>
2025-12-13 20:22:14 +0100

    Delete Datenanalyse_Outlier/statistic_analysis/basic.py

--- Datenanalyse_Outlier/statistic_analysis/basic.py
+++
@@ -1,9 +0,0 @@
-
-# ===================
-# 2. Basis-Analyse
-# ===================
-def basic_analysis(log):
-    print("->>>Verfügbare Spalten:", list(log.columns))
-    print("\n->>> Kopf der Daten:")
-    print(log.head())
-    print("\n------------------------")
