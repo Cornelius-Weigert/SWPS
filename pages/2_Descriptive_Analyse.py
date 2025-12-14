af27542e0eca4642ac5942307c5a666b3b6241e3
cornelius-weigert <cornelius.weigert@uni-bayreuth.de>
2025-12-14 15:45:11 +0100

    moved descriptive analysis into own page & away from main

--- pages/2_Descriptive_Analyse.py
+++ pages/2_Descriptive_Analyse.py
@@ -5,6 +5,12 @@ import Datenanalyse_Outlier.eventlog_to_image as eventlog_to_image
 import pm4py
 import pandas as pd
 from Datenanalyse_Outlier.map_columns import map_column
+from Datenanalyse_Outlier.display_analysis.frequency import show_frequency
+from Datenanalyse_Outlier.display_analysis.duration_process import show_process_duration
+from Datenanalyse_Outlier.display_analysis.resources import show_resources
+from Datenanalyse_Outlier.display_analysis.duration_activity import show_activity_duration
+from Datenanalyse_Outlier.display_analysis.standard_value import show_standard_compare
+from Datenanalyse_Outlier.display_analysis.duration_process import show_process_duration
 
 # --- SESSION STATE INITIALISIEREN ---
 if "uploaded_logs" not in st.session_state or st.session_state["uploaded_logs"] is None:
@@ -13,13 +19,33 @@ if "uploaded_logs" not in st.session_state or st.session_state["uploaded_logs"] 
 if not isinstance(st.session_state["uploaded_logs"], list):
     st.session_state["uploaded_logs"] = list(st.session_state["uploaded_logs"])
 
-st.session_state.setdefault("latest_upload", None)
-st.session_state.setdefault("file_path", None)
-st.session_state.setdefault("file_type", None)
-st.session_state.setdefault("file_name", None)
-st.session_state.setdefault("df", None)
-st.session_state.setdefault("log", None)
+df = st.session_state.get("df")
+log = st.session_state.get("log")
+
 # Sonstige Session States für Ausreißer
-st.session_state.setdefault("outlier_total", 0)
-st.session_state.setdefault("outlier_checked", 0)  
-st.session_state.setdefault("outliers_accepted", [])
+outlier_total = st.session_state.get("outlier_total")
+outlier_checked = st.session_state.get("outlier_checked")  
+outliers_accepted = st.session_state.get("outliers_accepted")
+
+tab1, tab2, tab3, tab4,tab5 = st.tabs([
+    "Standardwerte-Vergleich",
+    "Häufigkeit",
+    "Prozessdauer",
+    "Aktivitätsdauer",
+    "Ressourcen",])
+
+with tab1:
+    show_standard_compare(df)
+    
+with tab2:
+    show_frequency(df)
+
+with tab3:
+    show_process_duration(df)
+
+
+with tab4:
+    show_activity_duration(df)
+
+with tab5:
+    show_resources(df)
