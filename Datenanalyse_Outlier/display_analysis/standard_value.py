ef3127b1ad56af72692381dbb25f142789d5a292
yxnice22 <bt724620@uni-bayreuth.de>
2025-12-18 21:37:39 +0100

    Zeit-Einheit umsetzen,Standardwert separate ausgeben, Erklaerung von speziellen Begriffen

--- Datenanalyse_Outlier/display_analysis/standard_value.py
+++ Datenanalyse_Outlier/display_analysis/standard_value.py
@@ -2,6 +2,7 @@ import streamlit as st
 from ..statistic_analysis.standard_compare import compare_with_standardwert
 from ..statistic_analysis.duration_process import duration_pro_case 
 from ..statistic_analysis.duration_activity import duration_pro_activity
+from ..statistic_analysis.second_to_time import second_to_time
 
 def show_standard_compare(log, standard_dict=None,source_col="value"):
     """
@@ -15,23 +16,32 @@ def show_standard_compare(log, standard_dict=None,source_col="value"):
     """
     if log is None:
         st.warning("kein Eventlog geladen")
-        return
+        return    
     
     #Case Duration
     durations=duration_pro_case(log)
     if durations is not None:
         standard_duration=durations["case_duration"].mean()
-        df_standard= compare_with_standardwert(durations, standard_duration, value_col="case_duration")
+        df_standard= compare_with_standardwert(durations,standard_duration, value_col="case_duration")
+
+        # standard_duration_time=second_to_time(standard_duration)
+        df_standard["case_duration"]=df_standard["case_duration"].apply(second_to_time)
+        standard_duration_time=second_to_time(standard_duration)
         st.subheader("📊Case Duration: Standardwerte & Abweichungen")
+        st.write(f"🌟Standardwert(Durchschnitt) von Case-Dauer:{standard_duration_time}")
         st.dataframe(df_standard)
-     #st.dataframe(durations)
+    #  st.dataframe(durations)
     
     # Activity Duration
     activity_durations=duration_pro_activity(log)
     if activity_durations is not None:
         standard_activity = activity_durations["Activity_Duration"].mean()
         df_activity_standard = compare_with_standardwert(activity_durations, standard_activity, value_col="Activity_Duration")
+
+        df_activity_standard["Activity_Duration"]=df_activity_standard["Activity_Duration"].apply(second_to_time)
+        standard_activity_time=second_to_time(standard_activity)
         st.subheader("📊Activity Duration: Standardwerte & Abweichung")
+        st.write(f"🌟Standardwert(Durchschnitt) von Aktivität-Dauer:{standard_activity_time}")
         st.dataframe(df_activity_standard)
 
 
