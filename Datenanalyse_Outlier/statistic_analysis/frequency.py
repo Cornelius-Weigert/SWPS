73cb1328307bff2337932208448b7c7156518e07
yxnice22 <bt724620@uni-bayreuth.de>
2025-12-18 21:38:55 +0100

    unique_frequency hinzugefuegt

--- Datenanalyse_Outlier/statistic_analysis/frequency.py
+++ Datenanalyse_Outlier/statistic_analysis/frequency.py
@@ -10,3 +10,12 @@ def frequency1(log_df, event_col="activity"):
     freq = log_df[event_col].value_counts().reset_index()
     freq.columns = ["Event", "Häufigkeit"]
     return freq
+
+def frequency_unique(log_df, event_col="activity", case_col="case_id"):
+    """
+    Calculate the frequency of each unique event in the log
+    """
+    unique_freq = log_df.groupby(event_col)[case_col].nunique().reset_index()
+    unique_freq.columns = ["Event", "Unique_Häufigkeit"]
+    return unique_freq
+
